507cf9a68d10889debd22329a2162ec0242a32bf
hopefullman <1206892998@qq.com>
2020-03-07 14:00:05 +0800

    button css border

--- src/page/showAllActivityList/showAllActivityList.js
+++ src/page/showAllActivityList/showAllActivityList.js
@@ -45,7 +45,6 @@ class showAllActivityList extends Component{
               <div className="activity_title">
                 <h2><Icon type="clock-circle" theme="twoTone" twoToneColor="#52c41a"/>时间：{item.startTime}</h2>
               </div>
-              <div className="activity_content">简介：{item.content}</div>
             </div>
           </div>
         )
