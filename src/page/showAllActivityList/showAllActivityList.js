b9be650149c52feccbff129addaa035e1df6b0d5
hopefullman <1206892998@qq.com>
2020-03-28 11:51:47 +0800

    showallactivity 添加底部按钮

--- src/page/showAllActivityList/showAllActivityList.js
+++ src/page/showAllActivityList/showAllActivityList.js
@@ -1,6 +1,6 @@
 import React ,{Component} from 'react';
 import axios from 'axios';
-import { Icon } from 'antd';
+import { Icon ,Modal,Button} from 'antd';
 import {configUrl} from '../configUrl/configUrl.js';
 import './showAllActivityList.css';
 
