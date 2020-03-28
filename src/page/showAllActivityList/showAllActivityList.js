85df2a128aa51614dcf074c4a84cdf3fb8116bbe
hopefullman <1206892998@qq.com>
2020-03-28 11:31:06 +0800

    showallactivity 添加底部按钮

--- src/page/showAllActivityList/showAllActivityList.js
+++ src/page/showAllActivityList/showAllActivityList.js
@@ -8,9 +8,12 @@ class showAllActivityList extends Component{
   constructor(props){
     super(props);
     this.state={
-      activityList:[]
+      activityList:[],
+      visibles:false,
+      downloadQrcode:''
     }
   }
+  urls=''
   componentDidMount(){
     axios.get(`${configUrl}api/event/list`)
     .then((res)=>{
@@ -21,7 +24,37 @@ class showAllActivityList extends Component{
     .catch((err)=>{
       console.log('err activityList');
     })
+    let url=window.location.href;
+    this.urls=url.substring(0,url.indexOf("com")+4);
+    var u = navigator.userAgent;
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+    if(isiOS){
+        this.setState({
+          downloadQrcode:this.urls+'qrcodeios.png'
+        })
+    }else if(isAndroid){
+        this.setState({
+          downloadQrcode:this.urls+'qrcodeandroid.png'
+        })
+    }
   }
+  votings(){
+    this.setState({
+      visibles: true
+    });
+  }
+  handleOks = e => {
+    this.setState({
+      visibles: false
+    });
+  };
+  handleCancels = e => {
+    console.log(e);
+    this.setState({
+      visibles: false
+    });
+  };
   handleActivityItem(eventId){
     this.props.history.push(`/activityListDetail/${eventId}`);
   }
@@ -53,8 +86,29 @@ class showAllActivityList extends Component{
         )
       })
     }
-    
+    <div className='btn'> 
+      <Button type="primary" onClick={this.handleShowActivity.bind(this)}>查看所有活动</Button>
+      <Button type="danger" onClick={this.votings.bind(this)}>参与活动</Button>
     </div>
+    <Modal
+          title=<p style={{color:"#1890ff"}}>参加活动提醒</p>
+          visible={this.state.visibles}
+          onOk={this.handleOks}
+          onCancel={this.handleCancels}
+          okText="返回"
+          cancelText="关闭"
+        >
+        <div className="ZJL">
+          <div className="qrcode">
+            <div className="border">
+              <span>参赛方式</span>
+            </div>
+            <p>若您想参加大赛，请您在少儿画app中上传画作。识别二维码，下载注册登录少儿画app！</p>
+            <img src={`${this.state.downloadQrcode}`}/>
+            
+          </div>
+        </div>
+      </Modal>
   );
   }
 }
